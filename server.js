7cab813c8da72e0ea44992e39e0e8032960598f8
Abhinandan <your@email.com>
2026-02-13 12:48:34 +0530

    fix port for railway

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.get("/", async (req, res) => {
   }
 });
 
-const PORT = 3000;
-
 startScheduler();
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
